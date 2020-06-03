f17687834dfbd42470548eeca2aaa45dd1e57bba
Phokwane Malebye <phokwanemalebye@gmail.com>
2020-06-03 12:47:26 +0200

    Fill out form

--- contact_test.js
+++ contact_test.js
@@ -2,6 +2,14 @@ Feature('contact');
 
 Scenario('contact button', (I) => {
     I.amOnPage('https://www.dotmodus.com/');
-    I.see('Contact Us');
+    I.amOnPage('/about');
+    I.click('Schedule a session');
+    I.fillField('First Name', 'Phokwane');
+    I.fillField('Surname', 'Malebye');
+    I.fillField('Email', 'phokwane@dotmodus.com');
+    I.fillField('Contact Number', '1234567890');
+    I.fillField('Company', 'Dotmodus');
+    I.click('Submit');
+
 
 });
